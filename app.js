9cb713c8060c2433145c659d298b49cf85e2780c
杨尚融 <yangshangrong@yangshangrongdeMacBook-Pro.local>
2019-08-26 18:33:01 +1000

    fiest change

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = require('./router.js')
-var app = express();
-let bodyParser = require('body-parser');
+ let express = require('express');
+ let router = express.Router();
+ let bodyParser = require('body-parser');
+ let app = express();
 
-app.use(bodyParser.urlencoded({
+ app.use(bodyParser.urlencoded({
     extended: false
 }))
 
@@ -16,5 +16,34 @@ app.use(express.static('img'));
 app.set(express.static('view'));
 app.use(express.static('css'));
 
-app.use('/', router);
+
+//home page
+let dataBase = [];
+
+app.get('/', function(req,res){
+    res.render('index.html');
+});
+
+
+//new task
+app.get('/newtask', function(req,res){
+    res.render('newtask.html');
+});
+
+//list task
+app.get('/listtasks', function(req,res){
+    res.render('listtasks.html', {tasks: dataBase});
+});
+
+app.post('/data', function(req,res){
+    let data = {
+        taskname: req.body.taskname,
+        taskdue: req.body.taskdue,
+        taskdesc: req.body.taskdesc,
+    };
+    dataBase.push(data);
+    res.render('newtask.html');
+});
+
+
 app.listen("8080");
